0e47025a2e23dabb79c4b055af8366f4d19b9f20
Maryam <maryambalaghi01@gmail.com>
2025-05-03 17:24:38 +0330

    Add files via upload

---
+++ Dice_Combinations.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int count_way(int n)
+{
+    vector<int> dp(n+1,0);
+    dp[0]=1; // one way to make sum 0.
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=6;j++)
+        {
+            if ((i-j)>=0)
+            {
+                dp[i]+=dp[i-j];
+            }
+        }
+    }
+    return dp[n];
+}
+int main()
+{
+    int n;
+    cout<<"Please enter number:";
+    cin>>n;
+    cout<<count_way(n)<<" ways.";
+
+}
